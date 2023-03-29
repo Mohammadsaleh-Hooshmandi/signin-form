dc808e85b075b76346629d1175ca135304eee50a
Mohammdsale Hooshmandi <Mohammdsale.Hooshmandi1382@gmail.com>
2023-03-29 12:43:08 +0430

    add dark mode

--- script.js
+++ script.js
@@ -5,3 +5,10 @@ document.querySelector(".btn-rotate-1").addEventListener("click", () => {
 document.querySelector(".btn-rotate-2").addEventListener("click", () => {
     document.querySelector(".form-wrapper").classList.remove("rotate");
 });
+
+const icon = document.querySelector(".mode-btn i")
+document.querySelector(".mode-btn").addEventListener("click", () => {
+    const container = document.querySelector(".container");
+    container.classList.toggle("dark") ? icon.className = "fas fa-sun":
+    icon.className = "fas fa-moon";
+});
